1115385f958d2aca615f854cf5a0ffab417ef88f
Opliko <opliko.reg@protonmail.com>
2021-09-21 08:45:48 +0000

    just use a href

--- scripts/main.js
+++ scripts/main.js
@@ -4,23 +4,16 @@ const height = 2.7;
 function runScript() {
     const xInput = document.getElementById("x");
     const yInput = document.getElementById("y");
-    const calcButton = document.getElementById("calculate");
+    const button = document.getElementById("calculate");
     const result = document.getElementById("result");
-    const mailButton = document.getElementById("mailto");
-    if (calcButton) {
-        calcButton.addEventListener("click", () => {
-            const x = Number(xInput.value);
-            const y = Number(yInput.value);
-            const area = 2 * height * x + 2 * height * y;
-            const cost = area * price;
-            result.innerHTML = `Powierzchnia całkowita ścian: ${area} m<sup>2</sup><br>
-                                Koszt malowania: ${cost}zł`;
-        });
-    } else if (mailButton) {
-        mailButton.addEventListener("click", () => {
-            window.location.href = "mailto:remont@wp.pl";
-        });
-    }
+    button.addEventListener("click", () => {
+        const x = Number(xInput.value);
+        const y = Number(yInput.value);
+        const area = 2 * height * x + 2 * height * y;
+        const cost = area * price;
+        result.innerHTML = `Powierzchnia całkowita ścian: ${area} m<sup>2</sup><br>
+                            Koszt malowania: ${cost}zł`;
+    });
 }
 
 if (
