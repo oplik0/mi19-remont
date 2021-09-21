1a9fb4e76d19ccfe3de1cb5b59940e8acee5c466
Opliko <opliko.reg@protonmail.com>
2021-09-21 08:25:03 +0000

    finished?

--- scripts/main.js
+++ scripts/main.js
@@ -1,78 +1,20 @@
 "use strict";
-const zxcvbn_enabled = false;
+const price = 8;
 
-const runScript = async () => {
-    const options = {
-        translations: zxcvbnts["language-en"].translations,
-        graphs: zxcvbnts["language-common"].adjacencyGraphs,
-        dictionary: {
-            ...zxcvbnts["language-common"].dictionary,
-            ...zxcvbnts["language-en"].dictionary,
-        },
-    };
-    zxcvbnts.core.ZxcvbnOptions.setOptions(options);
-    const input = document.querySelector("#password-input");
-    const button = document.querySelector("#password-button");
-    const output = document.querySelector("#password-output");
-    const checkbox = document.querySelector("#zxcvbn-checkbox");
-    const checkPassword = async () => {
-        const zxcvbn_enabled = checkbox.checked;
-        const password = input.value;
-        const zxcvbnScore = zxcvbnts.core.zxcvbn(password);
-        const defaultScore =
-            -1 +
-            (password.length > 0) +
-            (password.length >= 4 && !!password.match(/\d/)) +
-            (password.length >= 7 && !!password.match(/\d/));
-        const score = zxcvbn_enabled ? zxcvbnScore.score : defaultScore;
-        let feedback = `<br>Złamanie tego hasła zajęło by ${zxcvbnScore.guesses} prób.
-        <br>Oznacza to ${zxcvbnScore.crackTimesSeconds.onlineNoThrottling10PerSecond} sekund ataku online (bez throttlingu)
-        <br>Albo ${zxcvbnScore.crackTimesSeconds.offlineFastHashing1e10PerSecond} sekund szybkiego ataku offline`;
-        if (
-            !!zxcvbnScore.feedback.suggestions.length ||
-            !!zxcvbnScore.feedback.warning.length
-        ) {
-            feedback += `<br>Ostrzeżenia: ${zxcvbnScore.feedback.warning}<br>Sugestie: ${zxcvbnScore.feedback.suggestions}`;
-        }
-        switch (score) {
-            case -1:
-                output.innerText = "WPISZ HASŁO!";
-                output.style.color = "red";
-                break;
-            case 0:
-                output.innerHTML = "SŁABE" + (zxcvbn_enabled ? feedback : "");
-                output.style.color = "yellow";
-                break;
-            case 1:
-                output.innerHTML = "ŚREDNIE" + (zxcvbn_enabled ? feedback : "");
-                output.style.color = "blue";
-                break;
-            case 2:
-                output.innerHTML = "DOBRE" + (zxcvbn_enabled ? feedback : "");
-                output.style.color = "green";
-                break;
-            case 3:
-                output.innerHTML =
-                    "BARDZO DOBRE" + (zxcvbn_enabled ? feedback : "");
-                output.style.color = "green";
-                break;
-            case 4:
-                output.innerHTML = "ŚWIETNE" + (zxcvbn_enabled ? feedback : "");
-                output.style.color = "green";
-                break;
-            default:
-                output.innerText = "TO NIE POWINNO SIĘ STAĆ";
-                output.style.color = "red";
-        }
-    };
-    button.addEventListener("click", checkPassword);
-    input.addEventListener("keyup", (event) => {
-        if (event.keyCode === 13) {
-            event.preventDefault();
-            checkPassword();
-        }
+function runScript() {
+    const xInput = document.getElementById("x");
+    const yInput = document.getElementById("y");
+    const button = document.getElementById("calculate");
+    const result = document.getElementById("result");
+    button.addEventListener("click", () => {
+        const x = Number(xInput.value);
+        const y = Number(yInput.value);
+        const area = x * y;
+        const cost = area * price;
+        result.innerHTML = `Powierzchnia całkowita ścian: ${area} m<sup>2</sup><br>
+                            Koszt malowania: ${cost}zł`;
     });
-};
+}
 
 if (
     document.readyState === "complete" ||
